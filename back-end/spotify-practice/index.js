d78c8f9a7ed17ec23033b46917e485881aeb66dd
gozieson <jgohwx@gmail.com>
2017-08-05 15:38:52 +1000

    something wrong with this
    can you help me?

--- back-end/spotify-practice/index.js
+++ back-end/spotify-practice/index.js
@@ -3,6 +3,7 @@ var SpotifyWebApi = require('spotify-web-api-node');
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
     clientId : '6c792eb4871e4c61832806d8e31aa947',
+    clientSecret : '82117e499eec43d3be315c27bc440b5f',
     redirectUri : 'http://www.example.com/callback'
 });
 
