f642692b92f7cc93dd7fd11e07bacc0ed340fc10
ccott1 <30653577+ccott1@users.noreply.github.com>
2017-08-05 17:38:20 +1000

    authorisationLine

--- back-end/spotify-practice/index.js
+++ back-end/spotify-practice/index.js
@@ -8,3 +8,6 @@ var spotifyApi = new SpotifyWebApi({
 });
 
 // spotifyApi.
+GET https:accounts.spotify.com/authorize/?client_id=6c792eb4871e4c61832806d8e31aa947&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback
+&scope=playlist-read-private playlist-modify-private playlist-modify-public user-library-read user-library-modify user-top-read user-follow-read
+&state=34fFs29kd09
