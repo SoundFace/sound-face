fcbf1dac4bbb0600b7c58b457241b1bdb82e0d51
Clarence <cksie1@student.monash.edu>
2017-08-05 23:24:45 +1000

    Fixed image capture
    Raw image data saved to lastImg

--- front-end/SoundFace/src/pages/home/home.ts
+++ front-end/SoundFace/src/pages/home/home.ts
@@ -8,6 +8,7 @@ import {
   CameraPreviewDimensions
 } from '@ionic-native/camera-preview';
 import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -18,32 +19,42 @@ export class HomePage {
   public lastImg: string;
   public myEmotion: string;
 
+  captureDataUrl: string;
+
   constructor(public navCtrl: NavController,
     private camera: Camera,
     private cameraPreview: CameraPreview,
     private http: Http) {
-    this.http.post('http://36666ac9.ngrok.io/processImage', "GetSchwifty");
-    this.initCamera();
+    this.startCamera();
   }
 
-  initCamera() {
-    console.log("initCamera called");
-    this.cameraPreview.startCamera({
-      x: 0,
-      y: 0,
-      width: window.innerWidth,
-      height: window.innerHeight-64,
-      camera: 'front',
-      toBack: false,
-      previewDrag: false,
-      tapPhoto: false
-    });
+  startCamera(){
+        // let react = {x: 40, y: 100, width: this.calcWidth ,height: 220}   //Decrepted due to previous code
+    this.cameraPreview.startCamera({x: 0, y: 0, width: window.innerWidth, height: window.innerHeight-64, toBack: false, previewDrag: false, tapPhoto: true});
+        //.startCamera(react, defaultCamera:'back',tapEnabled: true, dragEnabled: true, toBack:true, alpha:1);  //Decrepeted        
   }
 
   takePicture() {
-    this.http.post('http://36666ac9.ngrok.io/processImage', "Pringle");
     console.log("here");
-    this.cameraPreview.takePicture(function(imgData) {
+    this.camera.getPicture({
+      quality: 40,
+      destinationType: 0,
+      encodingType: 0,
+      sourceType: 1,
+      mediaType: 0,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      saveToPhotoAlbum: true
+    }).then((imageData) => {
+      //let base64Image = 'data:image/jpeg;base64,' + imageData;
+      let cameraImageSelector = document.getElementById('view');
+      this.lastImg = 'data:image/jpeg;base64,' + imageData;
+      cameraImageSelector.setAttribute('src', this.lastImg);
+      console.log(this.lastImg);
+      this.postRequest(this.lastImg);
+    });
+    /*
+      function(imgData) {
       this.cameraPreview.hide();
       // the img in string format: 'data:image/jpeg;base64,' + imgData
       // FREEZE IMAGE AFTER TAKING PIC FOR 2.5 SECONDS
@@ -52,20 +63,25 @@ export class HomePage {
       (<HTMLInputElement>document.getElementById('view')).src = lastImg;
       this.cameraPreview.show();
       //this.myEmotion = this.sendImage(lastImg);
-      this.http.post('https://36666ac9.ngrok.io/processImage', lastImg);
-    });
-  }
-  /*
-  sendImage(image) {
-    return this.http.post('https://36666ac9.ngrok.io/processImage', image);
-  }*/
-
-  testPost() {
-    this.http.post('https://36666ac9.ngrok.io/processImage', "GetSchwifty");
+      this.postRequest(imgData);
+    }); */
   }
 
-  completeAnalysis() {
-    // Handy's "after detection" function
+  // FROM AN EXAMPLE
+  postRequest(image) {
+    //let hello = 'data:image/jpeg;base64,' + String(image);
+    console.log(image);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let body = {
+      img : image
+    };
+
+    this.http.post("http://2203bb20.ngrok.io/processImage", JSON.stringify(body), {headers : headers})
+      .map(res => res.json())
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 
 }
